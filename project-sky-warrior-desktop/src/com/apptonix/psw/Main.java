856cfa77cef08ab38395b34f9f03ca854ea8bfcb
bradbird1990 <brad@Brads-MacBook-Pro.local>
2014-03-01 22:14:20 +0000

    Initial commit

---
+++ project-sky-warrior-desktop/src/com/apptonix/psw/Main.java
@@ -0,0 +1,16 @@
+package com.apptonix.psw;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "project-sky-warrior";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new ProjectSkyWarrior(), cfg);
+	}
+}
