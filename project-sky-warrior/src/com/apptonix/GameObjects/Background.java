2aa4f352cfea3e11b56cfc72dd3448e5bf2035fb
bradbird1990 <brad@Brads-MacBook-Pro.local>
2014-03-03 00:37:48 +0000

    Created an inherited background class for the scrollable class

---
+++ project-sky-warrior/src/com/apptonix/GameObjects/Background.java
@@ -0,0 +1,11 @@
+package com.apptonix.GameObjects;
+
+public class Background extends Scrollable {
+	
+	public Background(float x, float y, int width, int height, float scrollSpeed) {
+		
+		super(x, y, width, height, scrollSpeed);
+		
+	}
+
+}
